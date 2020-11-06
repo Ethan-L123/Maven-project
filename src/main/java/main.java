e22fe3cb1e4d50b3ffe800c5b849c88888aa740f
Student   LEITCH Ethan <06436@farnborough.ac.uk>
2020-11-06 11:00:56 +0000

    Initial commit

---
+++ src/main/java/main.java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main(String[] args) {
+        String word = "Hello there";
+
+        System.out.println(word);
+
+        System.out.println("1234567890");
+
+        System.out.println(word.length());
+    }
+}
